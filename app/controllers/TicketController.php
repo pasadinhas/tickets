ace054624e2cc5c0cb42f72ce524373e18a41164
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2014-06-24 01:21:11 +0100

    minor changes

--- app/controllers/TicketController.php
+++ app/controllers/TicketController.php
@@ -17,34 +17,12 @@ class TicketController extends BaseController {
 
     public function store() {
         // produces new ticket
-        // FIXME: ticket code is hardcoded!
-        if (Input::has('place')) {
+        if (Input::has('place', 'code')) {
             return Ticket::create(array(
                 'place_id' => Input::get('place'),
-                'code' => '26'
+                'code' => Input::get('code')
             ));
         }
-    }
-
-    public function getTicketsWaitingForPlace($place_id) {
-        return Ticket::where('place_id', $place_id)
-            ->has('service', false)
-            ->get();
-    }
-
-    public function getNextTicketForPlace($place_id) {
-        return Ticket::where('place_id', $place_id)
-            ->has('service', false)
-            ->orderBy('created_at', 'asc')
-            ->first();
-    }
-
-    public function getNewTicketForPlace($place_id) {
-        $t = new Ticket();
-        $t->place_id = $place_id;
-        $t->code = generate_ticket_code($place_id);
-        $t->save();
-        return $t;
     }
 
 }
